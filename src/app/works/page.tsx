68dd69a84a486d4c0da3ea0f40feecec016eb535
pierre_cours <pierre_cours@MacBook-Pro-80.local>
2024-07-09 11:18:07 +0200

    [Works] :bento: :bug: Fix Image

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -17,9 +17,9 @@ interface Work {
 }
 
 const works: Work[] = [
-    { id: '1', title: 'Work 1', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eaque pariatur atque, ut dolor quam laborum omnis at voluptatem vero magni, excepturi, tempora ratione et distinctio. Hic, reprehenderit. Sit, eum?', image: 'https://images.pexels.com/photos/26754397/pexels-photo-26754397/free-photo-of-mer-aube-paysage-soleil-couchant.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
-    { id: '2', title: 'Work 2', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eaque pariatur atque, ut dolor quam laborum omnis at voluptatem vero magni, excepturi, tempora ratione et distinctio. Hic, reprehenderit. Sit, eum?', image: 'https://images.unsplash.com/photo-1719857090179-aa689c5d7434?q=80&w=2769&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
-    { id: '3', title: 'Work 3', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eaque pariatur atque, ut dolor quam laborum omnis at voluptatem vero magni, excepturi, tempora ratione et distinctio. Hic, reprehenderit. Sit, eum?', image: 'https://images.unsplash.com/photo-1720209388220-a26fbff2a64f?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+    { id: '1', title: 'Work 1', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eaque pariatur atque, ut dolor quam laborum omnis at voluptatem vero magni, excepturi, tempora ratione et distinctio. Hic, reprehenderit. Sit, eum?', image: '/assets/images/landscape-01.webp' },
+    { id: '2', title: 'Work 2', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eaque pariatur atque, ut dolor quam laborum omnis at voluptatem vero magni, excepturi, tempora ratione et distinctio. Hic, reprehenderit. Sit, eum?', image: '/assets/images/landscape-02.webp' },
+    { id: '3', title: 'Work 3', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eaque pariatur atque, ut dolor quam laborum omnis at voluptatem vero magni, excepturi, tempora ratione et distinctio. Hic, reprehenderit. Sit, eum?', image: '/assets/images/landscape-01.webp' },
 ];
 
 const letterWrapper = (title: string) => {
@@ -79,6 +79,8 @@ export default function Works(): JSX.Element {
                             <Image 
                             src={work.image}
                             alt=""
+                            width={window.innerWidth}
+                            height={window.innerHeight}
                             />
                             <div className="overlay"></div>
                             <div className={styles.slide_texts_container}>
