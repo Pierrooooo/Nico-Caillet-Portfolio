7c62f5b73e2d8a9643240592ce02ccff6849bf62
pierre_cours <pierre_cours@MacBook-Pro-80.local>
2024-07-09 16:51:28 +0200

    [Works - FollowButton] ✨ Wip

--- src/app/follow-button.tsx
+++ src/app/follow-button.tsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
 const FollowButton: React.FC = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [opacity, setOpacity] = useState(0);
+  const parentRef = useRef<HTMLElement | null>(null);
 
   useEffect(() => {
     const handleMouseMove = (event: MouseEvent) => {
@@ -10,18 +12,38 @@ const FollowButton: React.FC = () => {
         y: event.pageY,
       });
     };
-  
-    window.addEventListener('pointermove', handleMouseMove);
-  
-    return () => {
+
+    const handleMouseEnter = () => {
+      window.addEventListener('pointermove', handleMouseMove);
+      setOpacity(1);
+    };
+
+    const handleMouseLeave = () => {
+      setOpacity(0);
       window.removeEventListener('pointermove', handleMouseMove);
     };
+    
+    const parentElement = document.querySelector('.section.section--slider');
+    console.log(parentElement)
+    if (parentElement) {
+      parentElement.addEventListener('mouseenter', handleMouseEnter);
+      parentElement.addEventListener('mouseleave', handleMouseLeave);
+    }
+
+    // return () => {
+    //   if (parentElement) {
+    //     parentElement.removeEventListener('mouseenter', handleMouseEnter);
+    //     parentElement.removeEventListener('mouseleave', handleMouseLeave);
+    //   }
+    //   window.removeEventListener('pointermove', handleMouseMove);
+    // };
   }, []);
-  
 
   const buttonStyle: React.CSSProperties = {
     top: position.y,
     left: position.x,
+    opacity: opacity,
+    transition: 'opacity 0.3s',
   };
 
   return (
